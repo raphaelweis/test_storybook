1656e5fa0105bbc17f4c0a32bedbbd30e3052c37
Raphaël Weis <raphael.weis@loreal.com>
2025-09-03 16:36:07 +0200

    feat: Added a label component with shadcn

---
+++ src/sharedComponents/ui/Label.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Label } from "@/sharedComponents/ui/Label";
+
+/**
+ * This is a simple and accessible label, generated using shadcn.
+ */
+const meta = {
+  title: "shadcn/Atoms/Label",
+  component: Label,
+  parameters: {
+    layout: "centered",
+    docs: {
+      subtitle: "This label is meant to showcase the main storybook features.",
+    },
+  },
+} satisfies Meta<typeof Label>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+/**
+ * The label is very simple, simply pass in any string as the children.
+ */
+export const Default: Story = {
+  args: {
+    children: "Storybook label.",
+  },
+};
+
